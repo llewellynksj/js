b8684de8ba5631d742e1c7dbd302785cbae64ab9
Kate L <llewellyn.ksj@gmail.com>
2024-10-01 12:46:29 +0100

    Update getquote function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 // Variables
 const theQuote = document.getElementById("the-quote");
+const theAuthor = document.getElementById("the-author");
+
 let randomQuote;
 
 function startGenerator() {
   loadButtons();
+  console.log("starting game");
+  getQuote(quotes);
 }
 
 /** Get a random word from the array */
 function getQuote(array) {
   randomQuote = array[Math.floor(Math.random() * array.length)];
+  theQuote.innerHTML = randomQuote.quote; // Display the quote
+  theAuthor.innerHTML = randomQuote.author;
+  console.log(randomQuote);
   return randomQuote;
 }
 
